e9cad296965017f3b2f238a8bbde6093421f4c69
cvaize <cvaize@gmail.com>
2021-10-07 14:39:24 +0500

    (Feat) Work

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,91 @@
+const mainTitle = "OG Swap";
+
+const shortDescription = "Authentic multichain swaps of native tokens via $GTON as a relay";
+
+const url = "https://mesh.susy.one";
+
+const previewImagePath = url+"/preview.jpg";
+
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'OG Swap',
+    title: mainTitle,
     htmlAttrs: {
       lang: "en-US"
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'OG Swap' },
+      { hid: 'description', name: 'description', content: shortDescription },
       { name: 'format-detection', content: 'telephone=no' },
+      {
+        name: "keywords",
+        content:
+          "blockchain, crosschain, interchain, oracles, datafeeds, waves, ethereum, bitcoin, gateways, consensus, smartcontract, ride, solidity"
+      },
+      {
+        content: mainTitle,
+        property: "og:title"
+      },
+      {
+        content: url,
+        property: "og:url"
+      },
+      {
+        content: "OG Swap",
+        property: "og:site_name"
+      },
+      {
+        property: "og:image",
+        content: previewImagePath
+      },
+      {
+        content: "OG Swap",
+        property: "twitter:site"
+      },
+      {
+        content: mainTitle,
+        property: "twitter:title"
+      },
+      {
+        property: "twitter:image",
+        content: previewImagePath
+      },
+      {
+        property: "twitter:image:src",
+        content: previewImagePath
+      },
+      {
+        property: "twitter:card",
+        content: "summary_large_image"
+      },
+      {
+        content: "all",
+        name: "robots"
+      },
+      {
+        content: "website",
+        property: "og:type"
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1"
+      },
+      {
+        name: "apple-mobile-web-app-status-bar-style",
+        content: "black"
+      },
+      {
+        name: "apple-mobile-web-app-capable",
+        content: "yes"
+      },
+      {
+        name: "format-detection",
+        content: "email=no"
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
